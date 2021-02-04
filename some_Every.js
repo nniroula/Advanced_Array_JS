108f937e895a1b7b85ba874632fdf5e547ffec13
Nabin <nkbhaie2017@gmail.com>
2021-02-04 10:31:45 -0700

    add  hasCertainKey every function

--- some_Every.js
+++ some_Every.js
@@ -68,4 +68,34 @@ console.log(hasNoDuplicates([1,2,3]));
 console.log(hasNoDuplicates([]));
 console.log(hasNoDuplicates([1,2,2]));
 
+/*
+        hasCertainKey
+Write a function called hasCertainKey which accepts an array of objects and a key, and returns 
+true if every single object in the array contains that key. Otherwise it should return false.
+let arr = [
+    {title: "Instructor", first: 'Elie', last:"Schoppik"},
+    {title: "Instructor", first: 'Tim', last:"Garcia", isCatOwner: true},
+    {title: "Instructor", first: 'Matt', last:"Lane"},
+    {title: "Instructor", first: 'Colt', last:"Steele", isCatOwner: true}
+  ]
+
+  hasCertainKey(arr,'first') // true
+  hasCertainKey(arr,'isCatOwner') // false
+*/
+function hasCertainKey(array, key){
+    return array.every(function(val){
+        if(val[key]){
+            return true;
+        }
+    });
+}
+let arr = [
+    {title: "Instructor", first: 'Elie', last:"Schoppik"},
+    {title: "Instructor", first: 'Tim', last:"Garcia", isCatOwner: true},
+    {title: "Instructor", first: 'Matt', last:"Lane"},
+    {title: "Instructor", first: 'Colt', last:"Steele", isCatOwner: true}
+  ]
+console.log(hasCertainKey(arr,'first'));
+console.log(hasCertainKey(arr,'isCatOwner'));
+
 
