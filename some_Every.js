16df1d987415c020ba3ec5c0b7826ee39a2d7a30
Nabin <nkbhaie2017@gmail.com>
2021-02-04 09:02:44 -0700

    add hasZero some function

--- some_Every.js
+++ some_Every.js
@@ -14,3 +14,22 @@ function hasOddNumber(array){
 }
 console.log(hasOddNumber([1,2,2,2,2,2,4]));
 console.log(hasOddNumber([2,2,2,2,2,4]));
+
+/*
+        hasAZero
+Write a function called hasAZero which accepts a number and returns true if that number 
+contains at least one zero. Otherwise, the function should return false
+hasAZero(33321232131012) // true
+hasAZero(1212121) // false
+*/
+function hasAZero(number){
+    let numberToString = number.toString();
+    let arrayFormed = numberToString.split('');
+    //console.log(typeof(arrayFormed)); // shows that arrayFormed is an object
+    return arrayFormed.some(function(val){
+        //console.log(val);
+        return val.indexOf(0) !== -1;
+    });
+}
+console.log(hasAZero(33321232131012));
+console.log(hasAZero(1212121));
