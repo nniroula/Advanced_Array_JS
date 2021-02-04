ad2a15c78d9766d05a3dafae3b74ed38431594ec
Nabin <nkbhaie2017@gmail.com>
2021-02-04 13:01:27 -0700

    add findIndex method

--- find_FindIndex.js
+++ find_FindIndex.js
@@ -12,6 +12,7 @@ const users = [
 findUserByUsername(users, 'mlewis') // {username: 'mlewis'}
 findUserByUsername(users, 'taco') // undefined
 */
+
 function findUserByUsername(array, key){
     return array.find(function(val){
         return val.username === key;
@@ -26,3 +27,35 @@ const users = [
 console.log(findUserByUsername(users, 'mlewis'));
 console.log(findUserByUsername(users, 'taco'));
 
+/*
+/*
+        removeUser
+Write a function called removeUser which accepts an array of objects, each with a key of 
+username, and a string. The function should remove the object from the array. If the object 
+is not found, return undefined.
+const users = [
+  {username: 'mlewis'},
+  {username: 'akagen'},
+  {username: 'msmith'}
+];
+
+removeUser(users, 'akagen') // {username: 'akagen'}
+removeUser(users, 'akagen') // undefined
+*/
+function removeUser(array, key){
+    let valueObtained = array.findIndex(function(val){
+        return val.username === key;
+    });
+    //console.log(valueObtained);
+    if(valueObtained === -1){
+        return undefined;
+    }
+    return array.splice(valueObtained,1)[0];
+}
+const users1 = [
+    {username: 'mlewis'},
+    {username: 'akagen'},
+    {username: 'msmith'}
+  ];
+console.log(removeUser(users1, 'akagen'));
+console.log(removeUser(users1, 'akagen'));
