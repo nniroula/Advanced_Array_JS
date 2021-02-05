0bef316e61c8afb20d5e97809ac6ad679d1797d7
Nabin <nkbhaie2017@gmail.com>
2021-02-04 18:26:24 -0700

    add vowelCount reduce method

--- reduce.js
+++ reduce.js
@@ -5,6 +5,7 @@ a new array with the value of each object at the key.
 const arr = [{name: 'Elie'}, {name: 'Tim'}, {name: 'Matt'}, {name: 'Colt'}]
 extractValue(arr,'name') // ['Elie', 'Tim', 'Matt', 'Colt']
 */
+
 function extractValue(array, key){
     // create an empty array, push object into it, and return accumulator.
     // make an empty array to be a second parameter so that it becomes accumulator
@@ -17,5 +18,39 @@ function extractValue(array, key){
 }
 const arr = [{name: 'Elie'}, {name: 'Tim'}, {name: 'Matt'}, {name: 'Colt'}]
 console.log(extractValue(arr,'name'));
+
+/*
+Write a function called vowelCount which accepts a string and returns an object with the keys 
+as the vowel and the values as the number of times the vowel appears in the string. This 
+function should be case insensitive so a lowercase letter and uppercase letter should count
+vowelCount('Elie') // {e:2,i:1};
+vowelCount('Tim') // {i:1};
+vowelCount('Matt') // {a:1})
+vowelCount('hmmm') // {};
+vowelCount('I Am awesome and so are you') // {i: 1, a: 4, e: 3, o: 3, u: 1};
+*/
+function vowelCount(str){
+    // Note: it does not return an array, rather returns things like string, or object
+    //let obj = {};  // this can be substituted by second parameter
+    let vowel = "aeiou";
+    let arrayFormed = Array.from(str);
+    //console.log(arrayFormed); // to check if arrayFormed holds correct data
+    return arrayFormed.reduce(function(accumulator, nextValue){
+        // hmm, it turns out to be case sensitive, use toLowerCase() method to fix it
+        if(vowel.indexOf(nextValue.toLowerCase()) !== -1){
+            if(accumulator[nextValue.toLowerCase()]){
+                accumulator[nextValue.toLowerCase()] += 1;
+            } else {
+                accumulator[nextValue.toLowerCase()] = 1;
+            }
+        }
+        return accumulator;
+    }, {});
+}
+console.log(vowelCount('Elie'));
+console.log(vowelCount('Tim'));
+console.log(vowelCount('Matt'));
+console.log(vowelCount('hmmm'));
+console.log(vowelCount('I Am awesome and so are you'));
 
 
